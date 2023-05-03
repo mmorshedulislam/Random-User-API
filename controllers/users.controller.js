025904a95b137d5c0b1f5d71251d7b877a41cad0
MORSHED-HOME <morshed952640@gmail.com>
2023-05-03 23:03:22 +0600

    delete method added

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,15 +1,24 @@
-const users = require("../views/users");
+let users = require("../views/users");
+
+module.exports = users;
 
 module.exports.getRandomUser = (req, res) => {};
 
 module.exports.getUsers = (req, res) => {
-  res.json(users);
+  res.status(200).json({
+    status: true,
+    data: users,
+  });
 };
 
 module.exports.saveUser = (req, res) => {};
 
 module.exports.updateUser = (req, res) => {};
 
-module.exports.deleteUser = (req, res) => {};
+module.exports.deleteUser = (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== Number(id));
+  res.send({ users });
+};
 
 module.exports.updateMultUser = (req, res) => {};
