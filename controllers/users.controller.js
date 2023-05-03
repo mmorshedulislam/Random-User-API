9531529cce01bd1b7baec144b27ef396e2651aec
MORSHED-HOME <morshed952640@gmail.com>
2023-05-04 00:31:27 +0600

    save user done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,24 +1,32 @@
 let users = require("../views/users");
 
-module.exports = users;
-
 module.exports.getRandomUser = (req, res) => {};
 
 module.exports.getUsers = (req, res) => {
   res.status(200).json({
-    status: true,
+    message: "success",
     data: users,
   });
 };
 
-module.exports.saveUser = (req, res) => {};
+module.exports.saveUser = (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.status(200).json({
+    message: "inserted new user",
+    data: user,
+  });
+};
 
 module.exports.updateUser = (req, res) => {};
 
 module.exports.deleteUser = (req, res) => {
   const { id } = req.params;
   users = users.filter((user) => user.id !== Number(id));
-  res.send({ users });
+  res.status(200).json({
+    message: "success",
+    data: users,
+  });
 };
 
 module.exports.updateMultUser = (req, res) => {};
